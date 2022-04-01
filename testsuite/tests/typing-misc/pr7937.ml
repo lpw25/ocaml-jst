99d4c336c6c28215475eca2950ba5a706f518da5
Leo White <leo@lpw25.net>
2022-04-01 17:41:17 +0100

    Keep generalized structure from patterns when typing let

--- testsuite/tests/typing-misc/pr7937.ml
+++ testsuite/tests/typing-misc/pr7937.ml
@@ -13,14 +13,6 @@ Line 3, characters 35-39:
 Error: This expression has type bool but an expression was expected of type
          ([< `X of int & 'a ] as 'a) r
        Types for tag `X are incompatible
-|}, Principal{|
-type 'a r = 'a constraint 'a = [< `X of int & 'a ]
-Line 3, characters 35-39:
-3 | let f: 'a. 'a r -> 'a r = fun x -> true;;
-                                       ^^^^
-Error: This expression has type bool but an expression was expected of type
-         ([< `X of 'b & 'a & 'c & 'd & 'e ] as 'a) r
-       Types for tag `X are incompatible
 |}]
 
 let g: 'a. 'a r -> 'a r = fun x -> { contents = 0 };;
@@ -30,14 +22,6 @@ Line 1, characters 35-51:
                                        ^^^^^^^^^^^^^^^^
 Error: This expression has type int ref
        but an expression was expected of type ([< `X of int & 'a ] as 'a) r
-       Types for tag `X are incompatible
-|}, Principal{|
-Line 1, characters 35-51:
-1 | let g: 'a. 'a r -> 'a r = fun x -> { contents = 0 };;
-                                       ^^^^^^^^^^^^^^^^
-Error: This expression has type int ref
-       but an expression was expected of type
-         ([< `X of 'b & 'a & 'c & 'd & 'e ] as 'a) r
        Types for tag `X are incompatible
 |}]
 
